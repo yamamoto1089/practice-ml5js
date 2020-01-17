f5abbdb1a6ef7577e76a966cca8d3aa01bd03e6f
ymmt1089 <ymmt1089@gmail.com>
2020-01-17 16:48:29 +0900

    完成せず

--- ml5-boilerplate-0.3.0-with-p5/sketch.js
+++ ml5-boilerplate-0.3.0-with-p5/sketch.js
@@ -1,36 +1,25 @@
-// Your code will go here
-// open up your console - if everything loaded properly you should see 0.3.0
-console.log('ml5 version:', ml5.version);
-
-function setup(){
-    createCanvas(400, 400);
-}
-
-function draw(){
-    background(200);
-
-}
-
 let imageFile = 'images/beer.jpg'; //画像ファイル名 let classifier; //画像分類器
 let img; //画像
-let status = ''; //表示テキスト
-function preload() { //画像を読み込み
-img = loadImage(imageFile); //モデルを読み込み
+let status = 'aaaa'; //表示テキスト
+function preload() {
+//モデルを読み込み
+classifier = ml5.imageClassifier('MobileNet'); //画像を読み込み
+img = loadImage(imageFile);
 }
 function setup() {
-//p5jsキャンバス生成 createCanvas(windowWidth, windowHeight); status = '画像分類中...'; //画像の分類開始
+//p5jsキャンバス生成 createCanvas(windowWidth, windowHeight); status = '画像分類中...'; //画像の分類開始 classifier.classify(img, gotResult);
 }
-function draw() {
-//画像を表示
-image(img, 0, 0, width, height); //分析結果をテキストで表示
-fill(255, 255, 127);
-  textSize(18);
+function draw() { //画像を表示
+    image(img, 0, 0, width, height); //分析結果をテキストで表示 fill(255, 255, 127); textSize(18);
   text(status, 20, 30);
-}
 // 解析結果の出力
 function gotResult(err, results) { // エラー処理
-    if (err) {
-    console.error(err); status = err;
-    }
+        if (err) {
+        console.error(err);
+            status = err;
+        }
     // 結果を出力
+    status =
+    'クラス名: ' + results[0].label
+    + ', 確度: ' + results[0].confidence + '¥n';
 }
